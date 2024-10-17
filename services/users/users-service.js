8e401d4cb4bf738be6af76a6e5f0406f609be0dd
ABISHEK-MITRAHSOFT <abishek.j@mitrahsoft.com>
2024-10-17 10:52:53 +0530

    Added image url in create,update for user

--- services/users/users-service.js
+++ services/users/users-service.js
@@ -27,11 +27,11 @@ module.exports = {
 
     createNewUser: (data, userId) => {
         const date = new Date();
-        const { first_name, last_name, email, role, address, country, state, city, } = data;
+        const { first_name, last_name, email, imageurl, role, address, country, state, city, } = data;
         return new Promise((resolve, reject) => {
-            const sql = `INSERT INTO users (id, userid, first_name, last_name, email, role, address, country, state, city, created_at, updated_at)
-            VALUES (uuid(),?,?,?,?,?,?,?,?,?,?,?)`;
-            pool.query(sql, [userId, first_name, last_name, email, role, address, country, state, city, date, date], (error, result) => {
+            const sql = `INSERT INTO users (id, userid, first_name, last_name, email, imageurl, role, address, country, state, city, created_at, updated_at)
+            VALUES (uuid(),?,?,?,?,?,?,?,?,?,?,?,?)`;
+            pool.query(sql, [userId, first_name, last_name, email, imageurl, role, address, country, state, city, date, date], (error, result) => {
                 if (error) {
                     return reject(error);
                 }
@@ -43,10 +43,10 @@ module.exports = {
     updateUserByUserId: (data, id) => {
         const date = new Date();
 
-        const { first_name, last_name, email, role, gender, address, country, state, city } = data;
+        const { first_name, last_name, email, imageurl, role, gender, address, country, state, city } = data;
         return new Promise((resolve, reject) => {
-            const sql = `UPDATE users SET first_name=?, last_name=?, email=?, role=?, gender=?, address=?, country=?, state=?, city=?, updated_at=? WHERE id=?`;
-            pool.query(sql, [first_name, last_name, email, role, gender, address, country, state, city, date, id], (error, result) => {
+            const sql = `UPDATE users SET first_name=?, last_name=?, email=?, imageurl=?, role=?, gender=?, address=?, country=?, state=?, city=?, updated_at=? WHERE id=?`;
+            pool.query(sql, [first_name, last_name, email, imageurl, role, gender, address, country, state, city, date, id], (error, result) => {
                 if (error) {
                     return reject(error);
                 }
