fc972aa67cb888f51baec06a0d3441a8ecc920f8
ABISHEK-MITRAHSOFT <abishek.j@mitrahsoft.com>
2024-10-18 12:08:12 +0530

    Added gender

--- services/users/users-service.js
+++ services/users/users-service.js
@@ -27,11 +27,11 @@ module.exports = {
 
     createNewUser: (data, userId) => {
         const date = new Date();
-        const { first_name, last_name, email, imageurl, role, address, country, state, city, } = data;
+        const { first_name, last_name, email, gender, imageurl, role, address, country, state, city, } = data;
         return new Promise((resolve, reject) => {
-            const sql = `INSERT INTO users (id, userid, first_name, last_name, email, imageurl, role, address, country, state, city, created_at, updated_at)
-            VALUES (uuid(),?,?,?,?,?,?,?,?,?,?,?,?)`;
-            pool.query(sql, [userId, first_name, last_name, email, imageurl, role, address, country, state, city, date, date], (error, result) => {
+            const sql = `INSERT INTO users (id, userid, first_name, last_name, email, gender, imageurl, role, address, country, state, city, created_at, updated_at)
+            VALUES (uuid(),?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+            pool.query(sql, [userId, first_name, last_name, email, gender, imageurl, role, address, country, state, city, date, date], (error, result) => {
                 if (error) {
                     return reject(error);
                 }
