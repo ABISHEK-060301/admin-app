40390fbbfdcfebc056401879016109b51a71fefe
ABISHEK-MITRAHSOFT <abishek.j@mitrahsoft.com>
2024-10-21 13:31:46 +0530

    Fixed error on getting all users by admin id

--- services/users/users-service.js
+++ services/users/users-service.js
@@ -15,7 +15,7 @@ module.exports = {
 
     getAllUsersByAdmin: (id) => {
         return new Promise((resolve, reject) => {
-            const sql = `SELECT * FROM users WHERE id = ?`;
+            const sql = `SELECT * FROM users WHERE userid = ?`;
             pool.query(sql, [id], (error, result) => {
                 if (error) {
                     return reject(error);
